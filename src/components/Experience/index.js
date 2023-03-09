13b02b297744b20111c45aca628adeafca1bd616
Alex Nash <Alexr.nash@gmail.com>
2023-03-09 17:20:24 -0500

    set up components

---
+++ src/components/Experience/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Experience = () => {
+  return (
+    <footer className="w-100 mt-auto text-dark p-4">
+      <div className="container text-center mb-5">
+        <h4></h4>
+      </div>
+    </footer>
+  );
+};
+export default Experience;

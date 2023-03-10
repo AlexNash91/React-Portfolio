530639fcace51be9ace924a0cb3a7d268ca8c53c
Alex Nash <Alexr.nash@gmail.com>
2023-03-09 21:08:52 -0500

    set up grid for projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 const Projects = () => {
   return (
-    <section id="work">
+    <section class="work">
       <h2 class="work-title">Projects</h2>
       <div class="container-1">
 
-        <p class="p1"></p>
+        <p class="p1">p1</p>
       </div>
       <div class="container-2">
 
-        <p class="p2"></p>
+        <p class="p2">p2</p>
       </div>
       <div class="container-3">
 
-        <p class="p3"></p>
+        <p class="p3">p3</p>
       </div>
       <div class="container-4">
 
-        <p class="p4"></p>
+        <p class="p4">p4</p>
       </div>
       <div class="container-5">
 
-        <p class=" p5"></p>
+        <p class="p5">p5</p>
       </div>
     </section>
   );
 };
+
 export default Projects;

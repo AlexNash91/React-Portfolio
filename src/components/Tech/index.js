e7a2bc65dce4f90782f154af77a980a5aff8fffa
Alex Nash <Alexr.nash@gmail.com>
2023-05-03 19:01:05 -0400

    fixed tech grid alignment issue

--- src/components/Tech/index.js
+++ src/components/Tech/index.js
@@ -3,7 +3,7 @@ const Tech = () => {
   return (
     <div className="w-100 mt-auto text-dark p-4">
       <div className="container text-center mb-5">
-        <section class="tech">
+        <section className="tech">
           <h2>Tech</h2>
           <ul class="grid">
             <li>HTML</li>
@@ -17,6 +17,7 @@ const Tech = () => {
             <li>Express</li>
             <li>React</li>
             <li>Python</li>
+            <li>C++</li>
           </ul>
         </section>
       </div>
